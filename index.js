d0217585ab85a6108a769678ae88c81350e0e9b0
TrungLe <trungle9995@gmail.com>
2023-11-16 21:59:23 -0500

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const elem = document.getElementById('main')
+elem.remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = "Trung is the champion"
+document.append(newHeader)
